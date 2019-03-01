777351f5a60023083248018b97329e614627de15
moriso-m <47560828+moriso-m@users.noreply.github.com>
2019-03-01 13:03:37 +0300

    deleted phone number

--- index.php
+++ index.php
@@ -6,5 +6,4 @@
  <body>
   <p>kcador will be avilable shortly.we are fixing a few things</p>
    <p>if you want to contact please do so via info@kcador.com</p>
-   <p>or via phone 07123456789
  </body>
